6bcc3b19e957a616d9aa3a49ce712ca023f56b8f
shagun Sodhani <sshagunsodhani@gmail.com>
2017-07-15 17:40:47 +0530

    Added code for both sequential and functional model

--- code/model.py
+++ code/model.py
@@ -1,9 +1,11 @@
-from keras.layers import LSTM, Merge, Activation, Input, \
-    Embedding, Dense, RepeatVector, TimeDistributed
-from keras.models import Sequential, Model
+from keras.layers import LSTM, Embedding, Dense, \
+    RepeatVector, TimeDistributed, Input
+from keras.layers import concatenate
+from keras.models import Model
 
 
-def _create_image_model(config_dict):
+def _create_image_model(config_dict,
+                        image_inputs):
     # image_model = Sequential()
     # image_model.add(Dense(units=config_dict['embedding_dim'],
     #                       input_dim=4096,
@@ -12,19 +14,17 @@ def _create_image_model(config_dict):
     # image_model.add(RepeatVector(n=config_dict['max_caption_length']))
     # return image_model
 
-    inputs = Input(shape=(4096,), name="image_model_input")
     x = Dense(units=config_dict['embedding_dim'],
               input_dim=4096,
               activation='relu',
-              name="image_model_fc")(inputs)
+              name="image_model_fc")(image_inputs)
     outputs = RepeatVector(n=config_dict['max_caption_length'],
                            name="image_model_repeatvector")(x)
-    return Model(inputs=inputs,
-                 outputs=outputs,
-                 name="image_model")
+    return outputs
 
 
-def _create_language_model(config_dict):
+def _create_language_model(config_dict,
+                           language_inputs):
     # language_model = Sequential()
     # language_model.add(Embedding(input_dim=config_dict['vocabulary_size'],
     #                              output_dim=256,
@@ -33,13 +33,10 @@ def _create_language_model(config_dict):
     # language_model.add(TimeDistributed(Dense(units=config_dict['embedding_dim'])))
     # return language_model
 
-    inputs = Input(shape=config_dict['max_caption_length'],
-                   name="language_model_input")
-
     x = Embedding(input_dim=config_dict['vocabulary_size'],
                   output_dim=256,
                   input_length=config_dict['max_caption_length'],
-                  name="language_model_embedding")(inputs)
+                  name="language_model_embedding")(language_inputs)
 
     x = LSTM(256,
              return_sequences=True,
@@ -47,36 +44,44 @@ def _create_language_model(config_dict):
 
     outputs = TimeDistributed(Dense(units=config_dict['embedding_dim']),
                               name="language_model_td")(x)
-
-    return Model(inputs=inputs,
-                 outputs=outputs,
-                 name="language_model")
+    return outputs
 
 
-def _create_merged_model(config_dict,
-                         image_model,
-                         language_model):
-    model = Sequential()
-    model.add(Merge([image_model, language_model], mode='concat'))
-    model.add(LSTM(1000, return_sequences=False))
-    model.add(Dense(units=config_dict['vocabulary_size']))
-    model.add(Activation('softmax'))
-    return model
+# def _create_merged_model(config_dict,
+#                          image_model,
+#                          language_model):
+#     print(image_model.summary())
+#     print(language_model.summary())
+#     model = Sequential()
+#     model.add(Merge([image_model, language_model], mode='concat'))
+#     model.add(LSTM(1000, return_sequences=False))
+#     model.add(Dense(units=config_dict['vocabulary_size']))
+#     model.add(Activation('softmax'))
+#
+#     print(model.summary())
+#     return model
 
 
 def create_model(config_dict,
-                 ret_model=False):
-    image_model = _create_image_model(config_dict=config_dict)
+                 compile_model=True):
+    image_inputs = Input(shape=(4096,), name="image_model_input")
+    image_model = _create_image_model(config_dict=config_dict,
+                                      image_inputs=image_inputs)
 
-    language_model = _create_language_model(config_dict=config_dict)
+    language_inputs = Input(shape=(config_dict['max_caption_length'],),
+                            name="language_model_input")
+    language_model = _create_language_model(config_dict=config_dict,
+                                            language_inputs=language_inputs)
 
-    merged_model = _create_merged_model(config_dict=config_dict,
-                                        image_model=image_model,
-                                        language_model=language_model)
-    if (ret_model == True):
-        return merged_model
-
-    merged_model.compile(loss='categorical_crossentropy',
-                         optimizer='rmsprop',
-                         metrics=['accuracy'])
-    return merged_model
+    merged_input = concatenate([image_model, language_model])
+    merged_input = LSTM(1000, return_sequences=False)(merged_input)
+    softmax_output = Dense(units=config_dict["vocabulary_size"],
+                           activation="softmax")(merged_input)
+    model = Model(inputs=[image_inputs,
+                          language_inputs], outputs=softmax_output)
+    print(model.summary())
+    if (compile_model == True):
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+    return model
